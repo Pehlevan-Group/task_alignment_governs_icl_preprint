3201858104216c36141ae653f1818a5b9f987544
Mary Letey <mletey@holylogin07.rc.fas.harvard.edu>
2025-09-17 15:18:09 -0400

    paranoia commit

--- NONLINEAR/experiment/remote/kappasweep_neurips/kappasweep.py
+++ NONLINEAR/experiment/remote/kappasweep_neurips/kappasweep.py
@@ -26,9 +26,9 @@ train_power = 0.9
 Ctr = np.diag(np.array([(j + 1) ** -train_power for j in range(d)])); Ctr = (Ctr/np.trace(Ctr))*d
 # Ctr = np.eye(d)
 
-trainobject = finitetasksampler(d, l, n, k, rho, Ctr)
-testobject_1 = fulltasksampler(d, l, n, rho, Ctr)
-testobject_2 = fulltasksampler(d, l, n, rho, np.diag(spikevalue(d, 0.5, 5)))
+trainobject = finitetasksampler(d, l, n, k, rho, Ctr, 2)
+testobject_1 = fulltasksampler(d, l, n, rho, Ctr, 2)
+testobject_2 = fulltasksampler(d, l, n, rho, np.diag(spikevalue(d, 0.5, 5)), 2)
 
 config = TransformerConfig(pos_emb=False, n_hidden=h, n_layers=2, n_mlp_layers=1, pure_linear_self_att=False)
 state, hist = train(config, data_iter=iter(trainobject), test_1_iter=iter(testobject_1), test_2_iter=iter(testobject_2), batch_size=16, loss='mse', test_every=100, train_iters=1000, optim=optax.adamw,lr=1e-4)
@@ -40,7 +40,7 @@ with open(file_path, 'wb') as fp:
 
 loss_func = optax.squared_error
 numsamples = 500
-testobject = fulltasksampler(d, l, n, rho, Ctr)
+testobject = fulltasksampler(d, l, n, rho, Ctr,2)
 tracker = []
 for _ in range(numsamples):
     xs, labels = next(testobject); # generates data
@@ -64,7 +64,7 @@ power_test_m = []
 power_test_s = []
 for test_power in test_powers:
     Ctest = np.diag(np.array([(j + 1) ** -test_power for j in range(d)])); Ctest = (Ctest/np.trace(Ctest))*d
-    testobject = fulltasksampler(d, l, n, rho, Ctest)
+    testobject = fulltasksampler(d, l, n, rho, Ctest,2)
     tracker = []
     for _ in range(numsamples):
         xs, labels = next(testobject); # generates data
@@ -88,7 +88,7 @@ exp_test_m = []
 exp_test_s = []
 for expower in expowers:
     Ctest = np.diag(np.array([np.exp(-expower*(j+1)) for j in range(d)])); Ctest = (Ctest/np.trace(Ctest))*d
-    testobject = fulltasksampler(d, l, n, rho, Ctest)
+    testobject = fulltasksampler(d, l, n, rho, Ctest,2)
     tracker = []
     for _ in range(numsamples):
         xs, labels = next(testobject); # generates data
@@ -112,7 +112,7 @@ rank_test_m = []
 rank_test_s = []
 for f in rankfs:
     Ctest = np.diag(complexity_class_covariance(d, int(d*f), True))
-    testobject = fulltasksampler(d, l, n, rho, Ctest)
+    testobject = fulltasksampler(d, l, n, rho, Ctest,2)
     tracker = []
     for _ in range(numsamples):
         xs, labels = next(testobject); # generates data
--- NONLINEAR/task/regression_structured.py
+++ NONLINEAR/task/regression_structured.py
@@ -12,19 +12,20 @@ import random
 
 # Implements DIVERSE ISOTROPIC case with C = I
 class fulltasksampler:
-    def __init__(self, d, l, n, rho, Ctask) -> None:
+    def __init__(self, d, l, n, rho, Ctask, single_index_power=1) -> None:
         self.d = d # DIMENSION
         self.l = l # CONTEXT LENGTH
         self.n = n # NUMBER OF CONTEXTS
         self.rho = rho # LABEL NOISE
         self.Ctask = Ctask # TASK COVARIANCE
         self.rng = np.random.default_rng(None) # SAMPLER OBJECT 
+        self.single_index_power = single_index_power
 
     def __next__(self):
         xs = self.rng.normal(loc=0, scale = 1/np.sqrt(self.d), size=(self.n, self.l + 1, self.d))
         ws = self.rng.multivariate_normal(mean=np.zeros(self.d), cov=self.Ctask, size=self.n)
         ws = ws[..., np.newaxis] 
-        ys = xs @ ws + self.rng.normal(loc=0, scale = np.sqrt(self.rho), size=(self.n, self.l+1, 1))
+        ys = (xs @ ws)**self.single_index_power + self.rng.normal(loc=0, scale = np.sqrt(self.rho), size=(self.n, self.l+1, 1))
         Z = np.zeros((self.n, self.l + 1, self.d + 1))
         Z[:,:,0:self.d] = xs
         Z[:,:,-1] = ys.squeeze()
@@ -39,7 +40,7 @@ class fulltasksampler:
 # We introduce a new parameter here as well
 # diversity: K = number of distinct beta_k to be sampled UNIFORMLY for each context 
 class finitetasksampler:
-    def __init__(self, d, l, n, k, rho, Ctask) -> None:
+    def __init__(self, d, l, n, k, rho, Ctask, single_index_power=1) -> None:
         self.d = d # DIMENSION
         self.l = l # CONTEXT LENGTH
         self.n = n # NUMBER OF CONTEXTS
@@ -50,14 +51,15 @@ class finitetasksampler:
         # Now we fix a set of TASKS which will be sampled from during all other calls to iter or next once this object is instantiated. 
         # Once we get to the actual sampling, we will use
         self.E = np.random.multivariate_normal(mean=np.zeros(self.d), cov=self.Ctask, size=self.k).T  # Shape: (D, K)
-    
+        self.single_index_power = single_index_power
+        
     def __next__(self):
         context_length = self.n + 1
         uniform_ps = np.array([random.randrange(self.k) for _ in range(self.n)])
         ws = np.array([self.E[:,uniform_ps[i]] for i in range(len(uniform_ps))]) 
         ws = ws[:,:,np.newaxis] # batch_size x n_dims x 1 as before
         xs = self.rng.normal(loc=0, scale = 1/np.sqrt(self.d), size=(self.n, context_length, self.d))
-        ys = xs @ ws + self.rng.normal(loc=0, scale = np.sqrt(self.rho), size=(self.n, context_length, 1))
+        ys = (xs @ ws)**self.single_index_power + self.rng.normal(loc=0, scale = np.sqrt(self.rho), size=(self.n, context_length, 1))
         Z = np.zeros((self.n, context_length, self.d + 1))
         Z[:,:,0:self.d] = xs
         Z[:,:,-1] = ys.squeeze()
